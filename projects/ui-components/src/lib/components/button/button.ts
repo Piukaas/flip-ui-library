3bc0102237a19a52f9a82bd4d46e49fef986d83b
Piukaas <thijsvanrijsbergen@gmail.com>
2026-04-02 21:50:10 +0200

    feat: color pallete

--- projects/ui-components/src/lib/components/button/button.ts
+++ projects/ui-components/src/lib/components/button/button.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from "@angular/core";
+import { ChangeDetectionStrategy, Component, input, InputSignal, ViewEncapsulation } from "@angular/core";
+
+type ButtonType = "button" | "submit" | "reset";
+type ButtonVariant = "primary" | "secondary" | "danger" | "warning" | "success" | "info" | "ghost";
 
 @Component({
   selector: "flip-button",
@@ -7,7 +10,16 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from "@angular/
   styleUrl: "./button.css",
   encapsulation: ViewEncapsulation.ShadowDom,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    "[attr.id]": "id()",
+    "[attr.data-variant]": "variant()",
+  }
 })
 export class Button {
-
+  public readonly id: InputSignal<string | undefined> = input();
+  public readonly type: InputSignal<ButtonType> = input<ButtonType>("button");
+  public readonly disabled: InputSignal<boolean> = input(true);
+  public readonly label: InputSignal<string | undefined> = input();
+  public readonly ariaLabel: InputSignal<string | undefined> = input();
+  public readonly variant: InputSignal<ButtonVariant> = input<ButtonVariant>("primary");
 }
